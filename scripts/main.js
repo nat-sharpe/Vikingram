2e2ff80e84e21094355b3c2a5e94ba944e9ddf4d
Nat Sharpe <sharpe1890@gmail.com>
2018-07-24 13:55:01 -0400

    now scrolls through images one way. lightbox closes when click outside box.

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
 
 
 var images = [
-    {caption: 'Me and the gang', url: "https://pmcvariety.files.wordpress.com/2014/02/vikings-tv-review.jpg?w=1000&h=563&crop=1"},
-    {caption: 'Cool ships', url: "https://prod-cdn-history-co-uk.s3.amazonaws.com/s3fs-public/vikings-main_0.jpg?jSru47BGLuEeljFBrRffvoPMkqfnyRzV"},
-    {caption: 'Babe', url: "https://heavyeditorial.files.wordpress.com/2015/02/cast-lagertha.jpg?quality=65&strip=all&w=780"},
-    {caption: 'Crazy friend with axes', url: "https://celebvogue.com/wp-content/uploads/2017/11/Vikings-Season-2-Floki-official-picture-vikings-tv-series-37651177-2655-3543-767x1024.jpg"},
-    {caption: 'Old dude I killed', url: "https://dawnsdressdiary.files.wordpress.com/2013/04/vikings3.jpg?w=640"},
+    {caption: 'Me and the gang', comments: 'Hanging out before the big battle is Wessex. Too much fun.', url: "https://pmcvariety.files.wordpress.com/2014/02/vikings-tv-review.jpg?w=1000&h=563&crop=1"},
+    {caption: 'Cool ships', comments: 'So cool... Floki is a genius. Check out those keels!', url: "https://prod-cdn-history-co-uk.s3.amazonaws.com/s3fs-public/vikings-main_0.jpg?jSru47BGLuEeljFBrRffvoPMkqfnyRzV"},
+    {caption: 'Babe', comments: 'What more can I say? ', url: "https://heavyeditorial.files.wordpress.com/2015/02/cast-lagertha.jpg?quality=65&strip=all&w=780"},
+    {caption: 'Crazy friend with axes', comments: "Don't mess with Floki. He's a great guy but sometimes the lights are on but nobody's home", url: "https://celebvogue.com/wp-content/uploads/2017/11/Vikings-Season-2-Floki-official-picture-vikings-tv-series-37651177-2655-3543-767x1024.jpg"},
+    {caption: 'Old dude I killed', comments: "The previous Earl. Love/hate him. Now he's in Valhalla", url: "https://dawnsdressdiary.files.wordpress.com/2013/04/vikings3.jpg?w=640"},
     {caption: 'My bro', url: "https://www.factinate.com/wp-content/uploads/2017/10/36-4.jpg"},  
     {caption: 'Fun day at the beach', url: "https://www.returndates.com/backgrounds/vikings.jpg"}, 
     {caption: 'New hairstyle', url: "https://cdn1.thr.com/sites/default/files/imagecache/gallery_landscape_887x500/2017/01/VikingsTVTravisFimmel_photofestH2017.jpg"},
@@ -18,17 +18,29 @@ var lightBox = document.querySelector('.modal');
 var popUp = document.querySelector('.modal-content');
 var close = document.querySelector('.close-button');
 var bigImage = document.querySelector('.big-image');
+var title = document.querySelector('.title');
+var comments = document.querySelector('.comments');
+
 
 var closeClick = function() {
-    lightBox.classList.toggle("show-modal");
+    lightBox.classList.remove("show-modal");
 };
 
 var nextImage = function(event) {
-    var index = event.currentTarget.getAttribute('data-index');
-    var newIndex = parseInt(index) + 1;
-    var image = images[newIndex];
+    var index = Number(event.currentTarget.getAttribute('data-index'));
+    var image = images[index + 1];
     bigImage.setAttribute('src', image.url);
+    bigImage.setAttribute('data-index', index + 1);
+    title.textContent = image.caption;
+    comments.textContent = image.comments;
 };
+
+function windowOnClick(event) {
+    console.log(event.target);
+    if (event.target === event.currentTarget) {
+        closeClick();
+    }
+}
 
 var handleClick = function(event) {
     event.preventDefault();
@@ -39,10 +51,16 @@ var handleClick = function(event) {
 
     lightBox.classList.toggle("show-modal");
 
-    close.addEventListener('click', closeClick);
     bigImage.addEventListener('click', nextImage);
+
+    title.textContent = image.caption;
+    comments.textContent = image.comments;
+
+    close.addEventListener('click', closeClick);
+
 };
 
+lightBox.addEventListener("click", windowOnClick);
 
 for (var index = 0; index < images.length; index++) {
 
