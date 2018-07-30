ab4a5860fa3d59ef45b5d54388b501265a158959
Nat Sharpe <sharpe1890@gmail.com>
2018-07-30 01:01:37 -0400

    tweaked fonts and fixed some bugs in the switch-list feature

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,7 @@ var postList = [
     {caption: '#family', comments: "Vápnum sínum skal-a maðr velli á feti ganga framar", url: "https://i.pinimg.com/736x/c2/1e/dc/c21edc322f978ad3dd3dcfe7a1b8ae44--vikings-tv-series-vikings-tv-show.jpg"},
     {caption: 'Battle-worn', comments: "At kveldi skal dag leyfa, konu, er brennd er, mæki, er reyndr er, mey, er gefin er, ís, er yfir kemr, öl, er drukkit er.", url: "https://cdna.artstation.com/p/assets/images/images/003/123/998/large/piotr-bieryt-screenshot029-copy.jpg?1469997592"},
     {caption: 'What do the gods intend?', comments: "Ósnjallr maðr hyggsk munu ey lifa, ef hann við víg varask; en elli gefr hánum engi frið, þótt hánum geirar gefi.", url: "https://tojuzbylo.pl/sites/default/files/v4_01_05152015_jh_02484-727x530.jpg"}, 
-    {caption: "Couldn't help myself", comments: "Deyr fé, deyja frændr, deyr sjalfr it sama, ek veit einn, at aldrei deyr: dómr um dauðan hvern.", url: "https://racheltsoumbakos.files.wordpress.com/2018/03/aslaug-as-portrayed-by-alyssa-sutherland-in-history-channels-vikings.jpg"},
+    {caption: "Femme fatale", comments: "Deyr fé, deyja frændr, deyr sjalfr it sama, ek veit einn, at aldrei deyr: dómr um dauðan hvern.", url: "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/main_element/public/media/image/2017/10/vikingos-temporada-5_1.jpg?itok=PV6g_vo0"},
     {caption: 'My former boss', comments: "At kveldi skal dag leyfa, konu, er brennd er, mæki, er reyndr er, mey, er gefin er, ís, er yfir kemr, öl, er drukkit er.", url: "https://dawnsdressdiary.files.wordpress.com/2013/04/vikings3.jpg?w=640"}
 ];
 
@@ -14,14 +14,14 @@ var taggedList = [
     {caption: 'How it all started', comments: 'Hvars þú böl kannt, kveð þú þér bölvi at ok gef-at þínum fjándum frið.', url: "http://www.journal.com.ph/media/k2/items/cache/afa65e44694e85c27c262d4077b83508_XL.jpg"},
     {caption: 'Tough love', comments: 'Hvars þú böl kannt, kveð þú þér bölvi at ok gef-at þínum fjándum frið.', url: "https://vikingmoviesinfo.files.wordpress.com/2014/01/vikings-s2-01.jpg"},
     {caption: "Hard day's work", comments: "At kveldi skal dag leyfa, konu, er brennd er, mæki, er reyndr er, mey, er gefin er, ís, er yfir kemr, öl, er drukkit er.", url: "https://vikingsnewsandrecaps.files.wordpress.com/2015/02/v-s03e01-5.jpg"},
-    {caption: 'The Lothbrok Bros',comments: "Deyr fé, deyja frændr, deyr sjalfr it sama, ek veit einn, at aldrei deyr: dómr um dauðan hvern.", url: "http://images6.fanpop.com/image/photos/34200000/Ragnar-and-Rollo-vikings-tv-series-34299776-500-333.jpg"},
+    {caption: 'Senior photo',comments: "Deyr fé, deyja frændr, deyr sjalfr it sama, ek veit einn, at aldrei deyr: dómr um dauðan hvern.", url: "https://cdn.shopify.com/s/files/1/1565/1123/files/ragnar_vikings_grimfrost_large.jpg?v=1481295399"},
     {caption: 'Feeling smug', comments: "Ósnjallr maðr hyggsk munu ey lifa, ef hann við víg varask; en elli gefr hánum engi frið, þótt hánum geirar gefi.", url: "https://i.pinimg.com/originals/3c/1e/f1/3c1ef159cff58486ab984f8063333b79.jpg"},
     {caption: 'Feeling nervous', comments: "At kveldi skal dag leyfa, konu, er brennd er, mæki, er reyndr er, mey, er gefin er, ís, er yfir kemr, öl, er drukkit er.", url: "https://vignette.wikia.nocookie.net/beyond-good-and-evil-burning-wheel/images/4/4f/Alexander_Frisk.jpg/revision/latest?cb=20131009031750"},
     {caption: 'Nothing like a cold one', comments: "Mörg er góð mær, ef görva kannar, hugbrigð við hali", url: "https://i.ytimg.com/vi/qgTAajmpNiA/hqdefault.jpg"},  
     {caption: "The English coin", comments: "Deyr fé, deyja frændr, deyr sjalfr it sama, ek veit einn, at aldrei deyr: dómr um dauðan hvern.", url: "http://www.patriciabracewell.com/wp-content/uploads/2016/12/4.12.Coin_-1.jpg"}, 
     {caption: 'Flirting with danger', comments: 'Bú er betra, þótt lítit sé, halr er heima hverr; þótt tvær geitr eigi ok taugreftan sal, þat er þó betra en bæn.', url: "https://i.pinimg.com/originals/81/62/1b/81621b0da89aa4502c3e0c444d80e43d.jpg"}
 ];
-
+var body = $('body');
 var mainContent = $('.main-content');
 var postButton = $('.post-button');
 var taggedButton = $('.tagged-button');
@@ -33,11 +33,13 @@ var modalComments = $('.modal-comments');
 var modalCloseButton = $('.close-button');
 var arrowLeft = $('.arrow-left');
 var arrowRight = $('.arrow-right');
+
 var currentImageIndex = 0;
+var currentList = postList;
 
 var changeModalImage = function(direction) {
     var i = currentImageIndex;
-    var neighborImage = postList[i + direction];
+    var neighborImage = currentList[i + direction];
     modalImage.attr('src', neighborImage.url);
     modalTitle.text(neighborImage.caption);
     modalComments.text(neighborImage.comments);
@@ -46,7 +48,7 @@ var changeModalImage = function(direction) {
 
 var clickCloseModal = function() {
     modal.removeClass("show-modal");
-
+    body.removeClass("stop-scrolling");
 };
 
 var clickModalBackground = function (event) {
@@ -56,7 +58,7 @@ var clickModalBackground = function (event) {
 };
 
 var nextImage = function () {
-    if (currentImageIndex === (postList.length - 1)) {
+    if (currentImageIndex === (currentList.length - 1)) {
         currentImageIndex = 0;
         changeModalImage(0);
     }
@@ -67,7 +69,7 @@ var nextImage = function () {
 
 var previousImage = function () {
     if (currentImageIndex === 0) {
-        currentImageIndex = (postList.length - 1);
+        currentImageIndex = (currentList.length - 1);
         changeModalImage(0);
     }
     else {
@@ -86,11 +88,13 @@ var arrowKeys = function (event) {
 
 var toggleImages = function () {
     if (event.target === postButton[0]) {
-        chooseList(postList);
+        currentList = postList;
     }
     else if (event.target === taggedButton[0]) {
-        chooseList(taggedList);    
+        currentList = taggedList;
     }
+    chooseList(currentList);    
+
 };
 
 modal.on('click', clickModalBackground);
@@ -128,6 +132,7 @@ var chooseList = function (whichList) {
         var clickOpenModal = function() {
             modalImage.attr('src', image.url);
             modal.addClass("show-modal");
+            body.addClass("stop-scrolling");
             modalTitle.text(image.caption);
             modalComments.text(image.comments);
             modalCloseButton.on('click', clickCloseModal);
@@ -140,4 +145,4 @@ var chooseList = function (whichList) {
     whichList.forEach(showList);
 }
 
-chooseList(postList);
+chooseList(currentList);
